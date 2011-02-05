ef6cdb1c2cea64cf3f8c6a9495ca5d56fd6d1931
Jeff Muizelaar <jrmuizel@theflash.(none)>
2011-02-05 16:22:01 -0500

    Try to avoid guessing the file

--- test.c
+++ test.c
@@ -105,13 +105,31 @@ void syntax_hilight(CXTranslationUnit TU, CXSourceRange full_range, char *file)
 	fclose(f);
 
 }
+
+void print_CXString(CXString str)
+{
+	printf("%s\n", clang_getCString(str));
+}
+
 int main(int argc, const char *const *argv)
 {
 	CXIndex Index = clang_createIndex(0, 0);
 	CXTranslationUnit TU = clang_parseTranslationUnit(Index, 0,
 		argv, argc, 0, 0, CXTranslationUnit_None);
-	
+
 	CXFile file = clang_getFile(TU, "test.c");
+#if 0
+	// this doesn't work because the root cursor doesn't seem to be 
+	CXCursor root_cursor = clang_getTranslationUnitCursor(TU);
+	//CXFile file = clang_getFile(TU, "test.c");
+	CXFile file;
+	CXSourceLocation root_location = clang_getCursorLocation(root_cursor);
+	if (clang_equalLocations(root_location, clang_getNullLocation())) {
+		printf("bad location");
+	}
+	clang_getInstantiationLocation(root_location, &file, NULL, NULL, NULL);
+	print_CXString(clang_getFileName(file));
+#endif
 	CXSourceLocation begin = clang_getLocation(TU, file, 1, 1);
 	struct source_position end_pos = get_endLocation("test.c");
 	CXSourceLocation end = clang_getLocation(TU, file, end_pos.line_no, end_pos.column_no);
